a176f7a9a7f6242e520f903606477c3240ba4ecf
Czichy <privat@czichy.com>
2018-05-10 13:31:28 +0200

    Update

--- 200-install-system-tools.sh
+++ 200-install-system-tools.sh
@@ -78,8 +78,8 @@ install_system_apps(){
 	POWERLINE_BASH_CONTINUATION=1
 	POWERLINE_BASH_SELECT=1
 	. /usr/lib/python3.6/site-packages/powerline/bindings/bash/powerline.sh" > ~/.bashrc
-  source ~/.bashrc
 
+	rmmod pcspkr
 
 
     echo "################################################################"
