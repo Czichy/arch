aa47e96ed10f8da465d57502982ef14f4b5e9ba5
czichy <privat@czichy.com>
2018-06-02 12:18:23 +0200

    clean

--- 300-install-themes.sh
+++ 300-install-themes.sh
@@ -8,17 +8,17 @@
 # fi
 #THEMES {{{
 install_themes(){
-  aur_package_install "arc-gtk-theme-git"
-  aur_package_install "conky-aureola"
-  aur_package_install "downgrade"
-  aur_package_install "inxi"
+  #aur_package_install "arc-gtk-theme-git"
+  #aur_package_install "conky-aureola"
+  #aur_package_install "downgrade"
+  #aur_package_install "inxi"
   aur_package_install "neofetch"
-  aur_package_install "numix-circle-icon-theme-git"
-  aur_package_install "pamac-aur"
-  aur_package_install "paper-icon-theme-git"
-  aur_package_install "papirus-icon-theme-git"
+  #aur_package_install "numix-circle-icon-theme-git"
+  #aur_package_install "pamac-aur"
+  #aur_package_install "paper-icon-theme-git"
+  #aur_package_install "papirus-icon-theme-git"
   aur_package_install "screenkey"
-  aur_package_install "surfn-icons-git"
+  #aur_package_install "surfn-icons-git"
 
   #git clone https://github.com/addy-dclxvi/gtk-theme-collections ~/.themes
   aur_package_install "vim-gruvbox-git vim-airline-gruvbox-git"
